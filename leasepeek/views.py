c423823fa7e61840ab594a1badb00b676b9d975a
andrewbantly <andrewbantly@gmail.com>
2023-09-06 12:16:41 -0700

    removed unnecessary imports

--- leasepeek/views.py
+++ leasepeek/views.py
@@ -1,27 +1,18 @@
-from django.shortcuts import render
 from .mongo_models import user_collection, data_collection
-from django.contrib.auth import get_user_model, login, logout
-from django.shortcuts import render
+from django.contrib.auth import login, logout
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
 from rest_framework import permissions, status
 from .validations import custom_validation, validate_email, validate_password
-from django.http import HttpResponse, HttpResponseRedirect
-from rest_framework.parsers import FileUploadParser
-import openpyxl
-# from .forms import DataForm
+from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
 from rest_framework.authentication import SessionAuthentication
 import pandas as pd
 from leasepeek.readers.xlsx_reader import read_rentroll
-import hashlib
 from bson.objectid import ObjectId
-# from .serializer_unit import UnitSerializer
-
 
 ###### USERS 
 
