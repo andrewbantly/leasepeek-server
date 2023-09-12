c4bccdd3381b0b40be3a540223aff2778387ee82
andrewbantly <andrewbantly@gmail.com>
2023-09-12 11:41:13 -0700

    file structuring

---
+++ leasepeek/readers/reader_functions/property_name.py
@@ -0,0 +1,14 @@
+def find_property_name(df):
+    print("Searching for property name")
+    keywords = ['Building', 'Property', 'Village', 'Apartment', 'District', 'North', 'East', 'West', 'South', 'Shadows', 'Place', 'Street', 'Aveneue', 'Circle', 'Court', 'Place']
+    property_name = ''
+    for i, row in df.iterrows():
+        row_str = ' '.join(row.dropna().astype(str))
+        # Check if any keyword exists in row_str
+        if any(keyword in row_str for keyword in keywords):
+            # Here, instead of assigning the keyword to building_name, 
+            # we want to assign the entire row string as it's the building's name.
+            property_name = row_str
+            break
+    print("Property:", property_name)
+    return property_name
