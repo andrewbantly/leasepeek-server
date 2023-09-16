d7051e24da8f824c456e27cd511d7c5412844ead
andrewbantly <andrewbantly@gmail.com>
2023-09-15 19:10:36 -0700

    process unit data can determine the unit data excel row range

--- leasepeek/readers/reader_functions/process_unit_data.py
+++ leasepeek/readers/reader_functions/process_unit_data.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
+keywords = ['Total', 'Totals', 'Totals:', 'Total Market Rent', 'Applications', 'Summary Groups', 'Future Residents']
+
 def process_unit_data(df, data_types):
     data_starting_row = data_types["Title Row"]
     data_types.pop("Title Row")
     print("")
-    print("starting row:", data_starting_row)
-    print("")
-    print("data titles:", data_types)
+
+    for i, row in df.iterrows():
+        cell_value = str(row.iloc[0])
+        if any(keyword in cell_value for keyword in keywords):
+            print(row)
+            print(i)
+            break
+
     print("")
