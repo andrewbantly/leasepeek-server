ab144a03d60311c259c5bcf09188fb4fd047ad55
andrewbantly <andrewbantly@gmail.com>
2023-10-03 12:18:04 -0700

    del unneeded import

--- leasepeek/urls/user_urls.py
+++ leasepeek/urls/user_urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .. import views
-from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
 	path('register', views.RegisterUserView.as_view(), name='register'),
