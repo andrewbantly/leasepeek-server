f9a36af45eb7a65347bf8204910c20c82e81fba2
andrewbantly <andrewbantly@gmail.com>
2023-10-18 19:19:47 -0700

    documentation

--- leasepeek/mongo_models.py
+++ leasepeek/mongo_models.py
@@ -1,3 +1,9 @@
+"""
+Database Connection Module for leasepeek_backend Project.
+
+This module establishes a connection to the MongoDB database and specifically targets the 'data' collection within the database. It leverages the connection settings defined in the `db_connection` module to interact with MongoDB.
+"""
+
 from db_connection import db
 
 data_collection = db['data']
