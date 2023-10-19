471790ace641a3e9ecb61266ca350d7ca46c94b6
andrewbantly <andrewbantly@gmail.com>
2023-10-18 19:10:12 -0700

    documentation

--- leasepeek/urls/user_urls.py
+++ leasepeek/urls/user_urls.py
@@ -1,7 +1,17 @@
+"""
+URL Configuration for user authentication views.
+
+This module contains URL patterns associated with user operations, specifically registration and login. It maps URLs to the appropriate views that handle user authentication processes.
+
+Available endpoints:
+- user/register: Endpoint for user registration. Connects to the RegisterUserView for user sign-up processes.
+- user/login: Endpoint for user login. Connects to the UserLoginView for existing user authentication.
+"""
+
 from django.urls import path
 from .. import views
 
 urlpatterns = [
-	path('register', views.RegisterUserView.as_view(), name='register'),
-	path('login', views.UserLoginView.as_view(), name='login'),
+    path('register', views.RegisterUserView.as_view(), name='register'),
+    path('login', views.UserLoginView.as_view(), name='login'),
 ]
