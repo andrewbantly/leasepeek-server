154d6be8b401e976b146480fed286bcdab3d38f5
andrewbantly <andrewbantly@gmail.com>
2023-10-20 14:05:04 -0700

    Secure SECRET_KEY with .env; update settings and refresh key

--- leasepeek_backend/settings.py
+++ leasepeek_backend/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fdv&e29^xc9!ixjq1vakx#4gzx=(k^xjs-9$mmw($7h@#e32pm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
