ddcade0974aa83c7bebbc6463858afa8a5af5d19
andrewbantly <andrewbantly@gmail.com>
2023-10-20 14:14:35 -0700

    environment changes to DB config

--- leasepeek_backend/settings.py
+++ leasepeek_backend/settings.py
@@ -14,14 +14,11 @@ from pathlib import Path
 from datetime import timedelta
 from decouple import config
 
-SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,18 +72,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'leasepeek_backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'snapple',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
