af7003d5931169d3281bbc809baca07f111280df
andrewbantly <andrewbantly@gmail.com>
2023-10-21 10:46:20 -0700

    improved vacancy report for rent roll that seperates vacancy with excel rows

--- leasepeek/readers/reader_functions/report_generators/vacancy.py
+++ leasepeek/readers/reader_functions/report_generators/vacancy.py
@@ -1,3 +1,7 @@
+
+vague_keywords =  {"Current/Notice/Vacant Residents",  "Future Residents/Applicants"}
+
+
 def vacancy(unit_data):
     
     # Initialize tenant status report that will be used if unit data includes a tenant status
@@ -8,16 +12,43 @@ def vacancy(unit_data):
     total_units = len(unit_data)
 
     for unit in unit_data:
-        if unit['status']:
+        if unit['status'] in vague_keywords:
+            if unit['status'] ==  "Future Residents/Applicants":
+                if 'Applicant' in tenant_status_report:
+                    tenant_status_report['Applicant'] += 1
+                else:
+                    tenant_status_report['Applicant'] = 1
+            else:
+                if 'vacant' in unit['tenant'].lower():
+                    print('add to vacant')
+                    if 'Vacant' in tenant_status_report:
+                        tenant_status_report['Vacant'] += 1
+                    else:
+                        tenant_status_report['Vacant'] = 1
+                elif unit['moveOut']:
+                    print('add to notice')
+                    if 'Notice' in tenant_status_report:
+                        tenant_status_report['Notice'] += 1
+                    else:
+                        tenant_status_report['Notice'] = 1
+                else:
+                    print('add to current')
+                    if 'Current' in tenant_status_report:
+                        tenant_status_report['Current'] += 1
+                    else:
+                        tenant_status_report['Current'] = 1
+
+        elif unit['status']: 
             if unit['status'] in tenant_status_report:
                 tenant_status_report[unit['status']] += 1
             else:
                 tenant_status_report[unit['status']] = 1
-
+                
         else:
             if 'vacant' in unit['tenant'].lower():
                 vacants += 1
-    
+
+
     if tenant_status_report:
         vacancy_data = tenant_status_report
     else: 
