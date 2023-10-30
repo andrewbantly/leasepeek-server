61329f3be3eee6e2e3b08d5ecf1849a481c8fe70
andrewbantly <andrewbantly@gmail.com>
2023-10-30 16:13:19 -0700

    added invalid password check

--- leasepeek/tests/test_views.py
+++ leasepeek/tests/test_views.py
@@ -129,15 +129,15 @@ class RegisterUserViewTest(APITestCase):
         # Ensure the error message matches the expected error structure
         self.assertEqual(json.loads(response.content), {'detail': ['Choose another email.']})
 
-    def test_register_user_no_emai(self):
+    def test_register_user_no_email(self):
         """
         Ensure we cannot register a user without an email address
         """
          # Create unique user data without a username
         user_register_data = {
-            "username": "",
+            "username": "testnoemail",
             "password": "testfail",
-            "email": "testnouser@test.com"
+            "email": ""
         }
 
         # Try to register a user without a username
@@ -147,7 +147,27 @@ class RegisterUserViewTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         # Ensure the error message matches the expected error structure
-        self.assertEqual(json.loads(response.content), {'detail': ['Choose another username.']})
+        self.assertEqual(json.loads(response.content), {'detail': ['Choose another email.']})
+
+    def test_register_user_invalid_password(self):
+        """
+        Ensure we cannot register a user without a valid password. The applications require a password to be eight characters or longer.
+        """
+        #  Create unique user data without a password
+        user_register_data = {
+            "username": "testnopassword",
+            "password": "",
+            "email": "testnopassword@test.com"
+        }
+
+        # Try to register a user without a password
+        response = self.client.post(self.register_url, user_register_data, format='json')
+
+        # The response should be a 400 status code indicating a bad request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Ensure the error message matches the expected error structure
+        self.assertEqual(json.loads(response.content), {'detail': ['Choose another password, min 8 characters.']})
 
 
     # More test cases can be added here to check for other edge cases 
