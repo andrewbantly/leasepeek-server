dcb291f4435d74ca5bee658847b58b020f326d8f
andrewbantly <andrewbantly@gmail.com>
2023-11-01 14:04:48 -0700

    added logger to test mongoDB connection

--- db_connection.py
+++ db_connection.py
@@ -1,8 +1,12 @@
 import pymongo
 import sys
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 if 'test' in sys.argv:
+    logger.info("Using Test MongoDB")
     url = 'mongodb://localhost:27017'
     client = pymongo.MongoClient(url)
     db = client['snapple_test']
