fa4e598ce6f95c1f4a5b6048e8b9d06d1f0c6ad1
andrewbantly <andrewbantly@gmail.com>
2023-11-01 14:22:28 -0700

    test no file upload 400 bad request response

--- leasepeek/tests/views/test_process_data.py
+++ leasepeek/tests/views/test_process_data.py
@@ -21,54 +21,26 @@ class ProcessDataViewTest(APITestCase):
         # create a user
         self.user = self.User.objects.create_user(username='testuserdataprocess', email='testuserdataprocess@test.com', password='testpass')
 
-    # Test good data with valid credentials
-    def test_process_valid_data(self):
-        user_data_payload = {
+        self.user_data_payload = {
             'email': 'testuserdataprocess@test.com',
             'password': 'testpass'
         }
 
+        self.BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    # Test valid file upload with valid credentials
+    def test_process_valid_data(self):
         # User needs to be logged in
-        login_response = self.client.post(self.login_url, user_data_payload, format='json')
+        login_response = self.client.post(self.login_url, self.user_data_payload, format='json')
 
         # Ensure the login response returns a 200 status code indicating login successful
         self.assertEqual(login_response.status_code, status.HTTP_200_OK)
 
-        # Ensure the login response includes username & email data
-        self.assertIn('username', login_response.data, "Username not found in the response data")
-        self.assertIn('email', login_response.data, "Email not found in the response data")
-
-        # Check if the registered user's username & email matches the response username & email
-        self.assertEqual(self.user.username, login_response.data["username"])
-        self.assertEqual(self.user.email, login_response.data["email"])
-
-        # Ensure the response does not include a user's password
-        self.assertNotIn('password', login_response.data, "Password is found in the response data")
-
-        # Ensure the login response includes an access token
-        self.assertIn('access', login_response.data, "Access token not found in the response data")
-        self.assertIn('refresh', login_response.data, "Refresh token not found in the response data")
-
         # Get the Access token from the response data
         access_token = login_response.data['access']
 
-        try:
-            # Decode the access token
-            access_token_obj = AccessToken(access_token)
-
-            # Username claim check
-            self.assertEqual(access_token_obj['username'], self.user.username, "Token's username does not match the created user's username")
-            
-        except jwt.ExpiredSignatureError:
-            self.fail("Access token is expired")
-        except jwt.InvalidTokenError:
-            self.fail("Invalid access token")
-
-        # Define the base directory for test data relative to this file's location
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
         # Construct the path to the test file
-        test_file_path = os.path.join(BASE_DIR, 'test_data', 'good_test_data.xlsx')
+        test_file_path = os.path.join(self.BASE_DIR, 'test_data', 'good_test_data.xlsx')
 
         # Open the file and read its content in binary mode
         with open(test_file_path, 'rb') as real_file:
@@ -76,7 +48,7 @@ class ProcessDataViewTest(APITestCase):
 
         # Create a SimpleUploadedFile with content read from the real file
         file = SimpleUploadedFile(
-                name="good_test_data",
+                name="good_test_data.xlsx",
                 content=file_content,
                 content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
@@ -86,54 +58,20 @@ class ProcessDataViewTest(APITestCase):
 
         # Check that the upload was successful
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        
-    def test_invalid_file(self):
-        user_data_payload = {
-            'email': 'testuserdataprocess@test.com',
-            'password': 'testpass'
-        }
-
+    
+    # Test invalid file type upload with valid credentials 
+    def test_invalid_file_type(self):
         # User needs to be logged in
-        login_response = self.client.post(self.login_url, user_data_payload, format='json')
+        login_response = self.client.post(self.login_url, self.user_data_payload, format='json')
 
         # Ensure the login response returns a 200 status code indicating login successful
         self.assertEqual(login_response.status_code, status.HTTP_200_OK)
 
-        # Ensure the login response includes username & email data
-        self.assertIn('username', login_response.data, "Username not found in the response data")
-        self.assertIn('email', login_response.data, "Email not found in the response data")
-
-        # Check if the registered user's username & email matches the response username & email
-        self.assertEqual(self.user.username, login_response.data["username"])
-        self.assertEqual(self.user.email, login_response.data["email"])
-
-        # Ensure the response does not include a user's password
-        self.assertNotIn('password', login_response.data, "Password is found in the response data")
-
-        # Ensure the login response includes an access token
-        self.assertIn('access', login_response.data, "Access token not found in the response data")
-        self.assertIn('refresh', login_response.data, "Refresh token not found in the response data")
-
         # Get the Access token from the response data
         access_token = login_response.data['access']
-
-        try: 
-            # Decode the access token
-            access_token_obj = AccessToken(access_token)
-
-            # Username claim check
-            self.assertEqual(access_token_obj['username'], self.user.username, "Token's username does not match the created user's username")
-        
-        except jwt.ExpiredSignatureError:
-            self.fail("Access token is expired")
-        except jwt.InvalidTokenError:
-            self.fail("Invalid access token")
-
-        # Define the base directory for test data relative to this file's location
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     
         # Construct the path to the invalid test file
-        test_file_path = os.path.join(BASE_DIR, 'test_data', 'csv_test_data.csv')
+        test_file_path = os.path.join(self.BASE_DIR, 'test_data', 'csv_test_data.csv')
 
         # Open the file and read its content in binary mode
         with open(test_file_path, 'rb') as real_file:
@@ -146,12 +84,25 @@ class ProcessDataViewTest(APITestCase):
             content_type="text/csv"
         )
     
-        # Send process data POST request
+        # Send process data POST request with invalid file type
         response = self.client.post(self.process_data_url, {'file': file}, format='multipart', HTTP_AUTHORIZATION=f'Bearer {access_token}')
 
         # Ensure the file upload response returns a 400 status code indicating a bad request
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
     
-    # test good data with invalid credentials
-    # test bad data with valid credentials 
-    # sent with no file
+    # Test no file upload with valid credentials
+    def test_no_file(self):
+        # User needs to be logged in
+        login_response = self.client.post(self.login_url, self.user_data_payload, format='json')
+
+        # Ensure the login response returns a 200 status code indicating login successful
+        self.assertEqual(login_response.status_code, status.HTTP_200_OK)
+
+        # Get the Access token from the response data
+        access_token = login_response.data['access']
+
+        # Send process data POST request with invalid file type
+        response = self.client.post(self.process_data_url, {}, format='multipart', HTTP_AUTHORIZATION=f'Bearer {access_token}')
+
+        # Ensure the file upload response returns a 400 status code indicating bad request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
