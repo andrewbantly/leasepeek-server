6e6b89d7f0fbab67a694921a0da458181d73b85b
andrewbantly <andrewbantly@gmail.com>
2023-11-04 10:19:15 -0700

    test read data refactor

--- leasepeek/tests/views/test_read_data.py
+++ leasepeek/tests/views/test_read_data.py
@@ -21,87 +21,68 @@ class ReadDataViewTest(APITestCase):
         # Create a User
         self.user = self.User.objects.create_user(username='testuserdataread', email='testuserdataread@test.com', password='testpass')
 
-        self.user_data_payload = {
-            'email': 'testuserdataread@test.com',
-            'password': 'testpass'
-        }
+        # Log in the test user and save the access token for authenticating further requests
+        login_response = self.client.post(self.login_url, {'email': 'testuserdataread@test.com', 'password': 'testpass'}, format='json')
+        self.access_token = login_response.data['access']
 
-        self.BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-        self.test_file_path = os.path.join(self.BASE_DIR, 'test_data', 'good_test_data.xlsx')
+    def upload_test_file(self, filename):
+        # Helper method to upload a test file using the saved access token
 
-        with open(self.test_file_path, 'rb') as real_file:
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        
+        test_file_path = os.path.join(BASE_DIR, 'test_data', filename)
+
+        with open(test_file_path, 'rb') as real_file:
             file_content = real_file.read()
 
-        self.file = SimpleUploadedFile(
-                name="good_test_data.xlsx",
+        file = SimpleUploadedFile(
+                name=f"{filename}",
                 content=file_content,
                 content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
+        
+        # Post the file to the processing endpoint with authentication
+        response = self.client.post(self.process_data_url, {'file': file}, format='multipart', HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+
+        response_data = json.loads(response.content)
+        objectId = response_data['objectId']
+
+        return objectId
 
     # Test read data 
     def test_read_data(self):
-        # An access token of a logged in user is required for the read data request
-        login_response = self.client.post(self.login_url, self.user_data_payload, format='json')
-        self.assertEqual(login_response.status_code, status.HTTP_200_OK)
-        access_token = login_response.data['access']
+        test_files = ['good_test_data.xlsx', 'private_test_data_01.xlsx','private_test_data_02.xlsx', 'private_test_data_03.xlsx', 'private_test_data_04.xlsx', 'private_test_data_05.xlsx', 'private_test_data_06.xlsx']
+        
+        for file in test_files:
+            # Upload the test file
+            objectId = self.upload_test_file(file)
 
-        # A objectId of a MongoDB document is required for the read data request
-        test_file_path = os.path.join(self.BASE_DIR, 'test_data', 'good_test_data.xlsx')
-        with open(test_file_path, 'rb') as real_file:
-            file_content = real_file.read()
-        file = SimpleUploadedFile(
-                name="good_test_data.xlsx",
-                content=file_content,
-                content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            )
-        data_upload_response = self.client.post(self.process_data_url, {'file': file}, format='multipart', HTTP_AUTHORIZATION=f'Bearer {access_token}')
-        self.assertEqual(data_upload_response.status_code, status.HTTP_201_CREATED)
-        response_data = json.loads(data_upload_response.content)
-        objectId = response_data['objectId']
+            # Send read data GET request with valid objectId and access token 
+            response = self.client.get(self.read_data_url, {'objectId': objectId}, HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
 
-        # Send read data GET request with valid objectId and access token 
-        data_read_response = self.client.get(self.read_data_url, {'objectId': objectId}, HTTP_AUTHORIZATION=f'Bearer {access_token}')
+            # Check that the data read was successful
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # Check that the data read was successful
-        self.assertEqual(data_read_response.status_code, status.HTTP_200_OK)
+            print(f"##### TEST FILE: {file} PASSED #####")
 
     # Test read data with invalid objectId
     def test_read_data_invalid_objectId(self):
-        # An access token of a logged in user is required for the read data request
-        login_response = self.client.post(self.login_url, self.user_data_payload, format='json')
-        self.assertEqual(login_response.status_code, status.HTTP_200_OK)
-        access_token = login_response.data['access']
-
         # A objectId of a MongoDB document is required for the read data request
         objectId = "03544c995cef54d4495facb4"
         
         # Send read data GET request with valid objectId and access token 
-        data_read_response = self.client.get(self.read_data_url, {'objectId': objectId}, HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.get(self.read_data_url, {'objectId': objectId}, HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
 
         # Check that the data read was unsuccessful
-        self.assertEqual(data_read_response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_read_data_invalid_access_token(self):
-        # A valid access token of a logged in user is required to add a MongoDB document object
-        login_response = self.client.post(self.login_url, self.user_data_payload, format='json')
-        access_token = login_response.data['access']
-
-        # A objectId of a MongoDB document is required for the read data request
-        test_file_path = os.path.join(self.BASE_DIR, 'test_data', 'good_test_data.xlsx')
-        with open(test_file_path, 'rb') as real_file:
-            file_content = real_file.read()
-        file = SimpleUploadedFile(
-                name="good_test_data.xlsx",
-                content=file_content,
-                content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            )
-        data_upload_response = self.client.post(self.process_data_url, {'file': file}, format='multipart', HTTP_AUTHORIZATION=f'Bearer {access_token}')
-        response_data = json.loads(data_upload_response.content)
-        objectId = response_data['objectId']
+        # Upload the test file
+        objectId = self.upload_test_file('good_test_data.xlsx')
 
         # Modify the access token
-        invalid_access_token = access_token.rsplit('.', 1)[0] + '.invalidsignature'
+        invalid_access_token = self.access_token.rsplit('.', 1)[0] + '.invalidsignature'
 
         # Send read data GET request with valid objectId and invalid access token 
         data_read_response = self.client.get(self.read_data_url, {'objectId': objectId}, HTTP_AUTHORIZATION=f'Bearer {invalid_access_token}')
