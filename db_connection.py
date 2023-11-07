1b479165aa779af9ca770efb4e4d5c51f32e1c64
andrewbantly <andrewbantly@gmail.com>
2023-11-06 19:50:50 -0800

    docker-compose runs and testing works

--- db_connection.py
+++ db_connection.py
@@ -1,17 +1,14 @@
-import pymongo
-import sys
+from pymongo import MongoClient
 import logging
+import sys
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-if 'test' in sys.argv:
-    logger.info("Using Test MongoDB")
-    url = 'mongodb://mongo:27017'
-    client = pymongo.MongoClient(url)
-    db = client['snapple_test']
-else:
-    url = 'mongodb://mongo:27017'
-    client = pymongo.MongoClient(url)
-    db = client['snapple']
+db_name = 'snapple_test' if 'test' in sys.argv else 'snapple'
+url = 'mongodb://mongo:27017'
 
+client = MongoClient(url)
+db = client[db_name]
+
+logger.info(f"Using MongoDB at {url} in database '{db_name}'")
--- leasepeek_backend/settings.py
+++ leasepeek_backend/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 from datetime import timedelta
-from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,11 +75,11 @@ WSGI_APPLICATION = 'leasepeek_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
@@ -164,3 +164,5 @@ SIMPLE_JWT = {
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
+
+print("DATABASE CONFIG:", DATABASES['default']['NAME'])
