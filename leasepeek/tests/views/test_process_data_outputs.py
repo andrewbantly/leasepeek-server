f726834bb323277600ed04390052d04056be35e0
andrewbantly <andrewbantly@gmail.com>
2023-11-14 14:56:56 -0800

    testing data outputs to assert processing logic

---
+++ leasepeek/tests/views/test_process_data_outputs.py
@@ -0,0 +1,50 @@
+import os
+import json
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
+from leasepeek.mongo_models import data_collection
+
+class ProcessDataViewValuesTest(APITestCase):
+    def setUp(self):
+        # Establish required end points
+        self.process_data_url = reverse('process_data')
+        self.login_url = reverse('login')
+
+        # Get the User model
+        self.User = get_user_model()
+  
+        # create a user
+        self.user = self.User.objects.create_user(username='testuserdataprocessvalues', email='testuserdataprocessvalues@test.com', password='testpass')
+
+        # Log in the test user and save the access token for authenticating further requests
+        login_response = self.client.post(self.login_url, {'email': 'testuserdataprocessvalues@test.com', 'password': 'testpass'}, format='json')
+        self.access_token = login_response.data['access']
+    
+    def upload_test_file(self, filename):
+        # Helper method to prepare a test file for upload using the saved access token
+
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        test_file_path = os.path.join(BASE_DIR, 'test_data', filename)
+
+        with open(test_file_path, 'rb') as real_file:
+            file_content = real_file.read()
+
+        file = SimpleUploadedFile(
+                name=f"{filename}",
+                content=file_content,
+                content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
+        
+        return file
+    
+    # Test valid file upload with valid credentials
+    def test_data_output_01(self):
+        file_name = os.environ.get('TEST_FILE_NAME_01')
+        file = self.upload_test_file(file_name)
+        response = self.client.post(self.process_data_url, {'file': file}, format='multipart', HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+        response_data = json.loads(response.content)
+        self.assertIn('message', response_data)
