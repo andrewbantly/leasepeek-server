a500bd105016c638e7e1023ffd93191ee22e10a2
andrewbantly <andrewbantly@gmail.com>
2023-11-20 14:50:50 -0800

    expiring leases logic update

--- leasepeek/readers/reader_functions/expiring_leases.py
+++ leasepeek/readers/reader_functions/expiring_leases.py
@@ -1,13 +1,6 @@
 from datetime import datetime, timedelta
 
-def expiring_leases(unit_data, as_of_date_str):
-    start = datetime.strptime(as_of_date_str, "%m/%d/%Y")
-    ninety_days_from_start = start + timedelta(days=90)
-    expiring_leases_count = 0
-    # print("Leases are expiring if before:", ninety_days_from_start)
-    # print()
-    
-    def determine_date_format(date_str):
+def determine_date_format(date_str):
         parts = date_str.split("/")
 
         if len(parts) != 3:
@@ -21,18 +14,46 @@ def expiring_leases(unit_data, as_of_date_str):
         else:
             raise ValueError("Unknown date format")
 
+def expiring_leases(unit_data, as_of_date_str):
+    as_of_date = datetime.strptime(as_of_date_str, "%m/%d/%Y")
+    ninety_days_from_as_of_date = as_of_date + timedelta(days=90)
+    expiring_leases_count = 0
+
+    expiring_time_windows = {
+        'expiring_in_90_days': {},
+        'expired': {}
+    }
+
+    floorplan_expiration_data = {}
+
+    # Initialize floorplan data for all floorplans in unit_data
     for unit in unit_data:
+        floorplan = unit['floorplan']
+        if floorplan not in floorplan_expiration_data:
+            floorplan_expiration_data[floorplan] = {
+                    'expiring_in_90_days': {'count': 0, 'total_rent': 0},
+                    'expired': {'count': 0, 'total_rent': 0}
+                }
+
+    for unit in unit_data:
+        floorplan = unit['floorplan']
         if unit['leaseExpire']:
             try:
                 expire_str = unit['leaseExpire'].replace('.', '/').replace('_', '/').replace('-', '/')
                 date_format = determine_date_format(expire_str)
                 lease_expire_date = datetime.strptime(expire_str, date_format)
-                if lease_expire_date >= start and lease_expire_date <= ninety_days_from_start:
-                    expiring_leases_count += 1
+
+                if lease_expire_date >= as_of_date and lease_expire_date <= ninety_days_from_as_of_date:
+                    floorplan_expiration_data[floorplan]['expiring_in_90_days']['count'] += 1
+                    floorplan_expiration_data[floorplan]['expiring_in_90_days']['total_rent'] += unit['rent']
+                elif lease_expire_date <= as_of_date:
+                    floorplan_expiration_data[floorplan]['expired']['count'] += 1
+                    floorplan_expiration_data[floorplan]['expired']['total_rent'] += unit['rent']
+            
             except ValueError:
                 # The date format is not valid or is empty
                 print(f"Can not determine lease expire of unit. Invalid date format for unit: {unit}")
-    print("expiring leases", expiring_leases_count)
 
+    print(floorplan_expiration_data)
 
-    return expiring_leases_count
+    return floorplan_expiration_data
