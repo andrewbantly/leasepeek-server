0819e915385ff2a32a746425354c5c5e6c5edb44
andrewbantly <andrewbantly@gmail.com>
2023-12-07 12:29:15 -0800

    removed nsf from all logic

--- leasepeek/readers/reader_functions/filter_personal_information.py
+++ leasepeek/readers/reader_functions/filter_personal_information.py
@@ -6,7 +6,6 @@ def filter_personal_info(unit_data):
         unit.pop('tenant')
         unit.pop('address')
         unit.pop('residentId')
-        unit.pop('nsf')
         unit.pop('unclassified')
 
     return unit_data
