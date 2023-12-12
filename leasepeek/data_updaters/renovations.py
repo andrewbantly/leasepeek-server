1ed88b4268efced3d44acd9cb1b594364a17a361
andrewbantly <andrewbantly@gmail.com>
2023-12-11 16:29:20 -0800

    refactor renovations updating logic

--- leasepeek/data_updaters/renovations.py
+++ leasepeek/data_updaters/renovations.py
@@ -8,79 +8,64 @@ def update_renovations_data(data):
     dataType = data.get('type')
 
     unit_data = data_collection.find_one({"_id": ObjectId(objectId)})
-
+    if not unit_data:
+        raise Exception(f"No document found with _id: {objectId}")
+    
     data_inputted_successfully = False
 
-    print(f'renovated units: {renovatedUnits}')
-    print(f'renovated floor plans: {renovatedFloorPlans}')
-    # form sent 
+    # Update unit data based on renovatedFloorPlans
     if dataType == 'floorPlan':
-        if unit_data and "floorplans" in unit_data:
-            for plan_name, plan_data in unit_data['floorplans'].items():
+        if "floorplans" in unit_data:
+            for plan_name in unit_data['floorplans']:
                 is_renovated = plan_name in renovatedFloorPlans
-
                 field_path = f"floorplans.{plan_name}.renovated"
-
                 update_query = {"$set": {field_path: is_renovated}}
                 result = data_collection.update_one({"_id": ObjectId(objectId)}, update_query)
-
                 if result.matched_count > 0:
                     data_inputted_successfully = True
                 else:
                     raise Exception("Data input error")
-        if unit_data and "data" in unit_data:
+                
+        if "data" in unit_data:
             for unit_info in unit_data['data']:                    
-                unit_id = unit_info['unit'] 
                 unit_floorplan = unit_info['floorplan']
                 is_renovated = unit_floorplan in renovatedFloorPlans
-                field_path = f"data.{unit_id}.renovated"
-
-                update_query = {
-                    "$set": {
-                        "data.$[elem].renovated": is_renovated
-                    }
-                }
-                array_filters = [
-                    {"elem.floorplan": unit_floorplan}  # Filter based on floorplan
-                ]
-
-                result = data_collection.update_one(
-                    {"_id": ObjectId(objectId)},
-                    update_query,
-                    array_filters=array_filters
-                )
+                field_path = f"data.{unit_info['unit']}.renovated"
+                update_query = {"$set": {"data.$[elem].renovated": is_renovated}}
+                array_filters = [{"elem.floorplan": unit_floorplan}]
+                result = data_collection.update_one({"_id": ObjectId(objectId)}, update_query, array_filters=array_filters)
             
             if result.matched_count > 0:
                 data_inputted_successfully = True
             else:
                 raise Exception("Data input error")
 
-        if data_inputted_successfully:
-            print("Data updated successfully")
-        else:
-            print("No data updated")
+    # Update unit data based on renovatedUnits
     else:
-        # for === unitNumber
-        print('unit number logic')
+        floorplans_to_update = []
+        if "data" in unit_data:
+            for unit_info in unit_data['data']:                    
+                unit_id = unit_info['unit'] 
+                is_renovated = unit_id in renovatedUnits
+                if is_renovated:
+                    floorplans_to_update.append(unit_info['floorplan'])
+                field_path = f"data.{unit_id}.renovated"
+                update_query = {"$set": {"data.$[elem].renovated": is_renovated}}
+                array_filters = [{"elem.unit": unit_id}]
+                result = data_collection.update_one({"_id": ObjectId(objectId)}, update_query, array_filters=array_filters)
+                if result.matched_count > 0:
+                    data_inputted_successfully = True
+                else:
+                    raise Exception("Data input error")
+        if "floorplans" in unit_data:
+            for plan_name in unit_data['floorplans']:
+                is_renovated = plan_name in floorplans_to_update
+                field_path = f"floorplans.{plan_name}.renovated"
+                update_query = {"$set": {field_path: is_renovated}}
+                result = data_collection.update_one({"_id": ObjectId(objectId)}, update_query)
+                if result.matched_count > 0:
+                    data_inputted_successfully = True
+                else:
+                    raise Exception("Data input error")
 
-
-
-
-
-    #     print('data type')
-    # elif dataType == "unitNumber":
-    #     print('data type unit Num')
-    # else:
-    #     print('errror')
-
-
-    #  data type: floorPlan
-    #  ['B3', 'E1A', 'A5']
-
-    # data type: unitNumber
-    # ['1-1302', '1-1306', '1-1308']
-
-    print()
-    print(f"true? {data_inputted_successfully}")
-
-    return 'success'
+    return "Document updated successfully." if data_inputted_successfully else "Data input error."
